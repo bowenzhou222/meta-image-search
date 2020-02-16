65c4fe3a2d92556f21df46b0b16d10bad430c616
Bowen <bowenzhou222@gmail.com>
2020-02-17 09:26:44 +1100

    update api url

--- src/config.ts
+++ src/config.ts
@@ -2,5 +2,5 @@ const env: 'development' | 'production' = process.env.REACT_APP_ENV as any;
 
 export const baseUrl: string = {
   development: 'http://localhost:9090',
-  production: 'http://18.216.213.41:9090',
+  production: 'https://meta-image-search-api.herokuapp.com',
 }[env];
