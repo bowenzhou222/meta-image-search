b4aae1e54d43d4f1a307318df7dbc60794c433cf
bowenzhou222 <bowenzhou222@gmail.com>
2020-02-16 13:01:30 +1100

    update baseUrl

--- src/config.ts
+++ src/config.ts
@@ -2,5 +2,5 @@ const env: 'development' | 'production' = process.env.REACT_APP_ENV as any;
 
 export const baseUrl: string = {
   development: 'http://localhost:9090',
-  production: 'https://placeholder',
+  production: 'http://18.216.213.41:9090',
 }[env];
